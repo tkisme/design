1889fdbce7a5126f76fc10bf4a135cbf3e876626
kevin <tkisme2013@gmail.com>
2016-01-19 08:54:39 +0800

    upgrade

---
+++ app/auth/forms.py
@@ -0,0 +1,70 @@
+# coding=utf-8
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
+
+
+class LoginForm(Form):
+    username = StringField(u'用户名', validators=[Required(), Length(1, 64)])
+    password = PasswordField(u'密码', validators=[Required()])
+    remember_me = BooleanField(u'保持登录')
+    submit = SubmitField(u'登录')
+
+
+class RegistrationForm(Form):
+    username = StringField(u'用户名', validators=[
+        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, u'用户名以字母开头+字母或数字或下划线')])
+    password = PasswordField(u'密码', validators=[
+        Required(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField(u'确认密码', validators=[Required()])
+    mobile = StringField(u'手机号', validators=[Required(u'请输入手机号'),
+                                             Regexp('^(1(3|5|7|8)[0-9]{9})$', 0, u'输入正确的手机号')])
+    submit = SubmitField(u'注册')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
+
+
+class ChangePasswordForm(Form):
+    old_password = PasswordField('Old password', validators=[Required()])
+    password = PasswordField('New password', validators=[
+        Required(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('Confirm new password', validators=[Required()])
+    submit = SubmitField('Update Password')
+
+
+class PasswordResetRequestForm(Form):
+    email = StringField('Email', validators=[Required(), Length(1, 64),
+                                             Email()])
+    submit = SubmitField('Reset Password')
+
+
+class PasswordResetForm(Form):
+    email = StringField('Email', validators=[Required(), Length(1, 64),
+                                             Email()])
+    password = PasswordField('New Password', validators=[
+        Required(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('Confirm password', validators=[Required()])
+    submit = SubmitField('Reset Password')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first() is None:
+            raise ValidationError('Unknown email address.')
+
+
+class ChangeEmailForm(Form):
+    email = StringField('New Email', validators=[Required(), Length(1, 64),
+                                                 Email()])
+    password = PasswordField('Password', validators=[Required()])
+    submit = SubmitField('Update Email Address')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
